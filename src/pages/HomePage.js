6c13945503ba74d39136878afd80142eda337b93
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-09-15 14:56:23 +0200

    Logo added to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,8 @@
+import logo from "../assets/logo_inverted.webp";
 export default function HomePage() {
     return (
-        <section className="page">
-            <h1>Home Page</h1>
+        <section className="page home">
+            <img src={logo} className="logo center" alt="Cederdorff Logo" />
         </section>
     );
 }
